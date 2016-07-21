e94bd5d726fc056382186b025b9c3eaf5832ecb8
Surya <itzmesurya@gmail.com>
2016-07-21 12:31:36 +0530

    made changes to server by adding express code and added stuff in index.html; also updated README.md file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ngPG
 
 Contains a sample app
+
+1. Created the folder structure.
+
+1. a. Added server.js file --> node server using express.  
--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 var express = require("express");
 var app = express();
 
+// creating a static location for the 
+// resources - node starts to look into
+// static paths
+// 1. adding "public" folder.
+app.use(express.static("public"));
 
+// adding a basic route
+app.get("/*", function (req, res) {
+    res.sendfile("./public/app/views/index.html");
+});
+
+// port to be used for listening
+var port = 5000;
+
+// listening to the port
+app.listen(port, function () {
+    console.log("Message from server.js: Listening on port : " + port);
+});
